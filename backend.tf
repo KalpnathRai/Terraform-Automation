dd96c04c1d648aaef97a20173a4679d5db9532c8
KalpnathRai <165119993+KalpnathRai@users.noreply.github.com>
2025-12-27 20:29:31 +0530

    Update Terraform backend key for state file

--- backend.tf
+++ backend.tf
@@ -1,11 +1,12 @@
 terraform {
   backend "s3" {
     bucket = "kalp-minds-app-project-terraform-state"
-    key = "main"
+    key = "main/terraform.tfstate"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
     }
 }
+
 
 
 
