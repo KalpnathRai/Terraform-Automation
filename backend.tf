87b49744c3d54c04a4f0f1fb003c2bedccc3a81b
KalpnathRai <165119993+KalpnathRai@users.noreply.github.com>
2025-12-27 18:34:58 +0530

    Update S3 backend configuration in backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "young-minds-app-project-terraform-state"
+    bucket = "kalp-minds-app-project-terraform-state"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
   }
 }
+
